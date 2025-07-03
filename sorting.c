73c9287125a21b17875f10afe17e2418a0c632a5
Himanigaud <himanigaud2006@gmail.com>
2025-07-03 14:30:37 +0000

    3/07

--- sorting.c
+++ sorting.c
@@ -4,7 +4,7 @@ void main(){
     int a[10]= {10,9,7,101,23,44,12,78,24,33};
     for(i=0;i<10;i++){
         for(j=i+1;j<10;j++){
-            if(a[j]>a[i]){
+            if(a[j]<a[i]){
                 temp=a[i];
                 a[i]=a[j];
                 a[j]=temp;
