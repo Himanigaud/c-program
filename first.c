8d42e362b04b86abc68f9d2d2c5f4573f7c09fde
Himanigaud <himanigaud2006@gmail.com>
2025-07-07 08:27:51 +0000

    7/07

---
+++ first.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+int main(){
+    int i,arr[5];
+    arr[0]=78;
+    arr[1]=65;
+    arr[2]=45;
+    arr[3]=57;
+    arr[4]=89;
+
+    for(i=0;i<5;i++);
+    printf("%d\t",arr[i]);
+
+}
