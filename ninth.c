ee2cbd377b6c274697a6219641faa7b463e73800
Himanigaud <himanigaud2006@gmail.com>
2025-07-13 09:12:45 +0000

    12/07

---
+++ ninth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int number=50;
+    int*p;
+    p=& number;
+    printf("address rhe p variable %u\n ",p);
+    p=p+1;
+    printf("after increment:address the p variable %u\n",p);
+    return 0;
+
+}
