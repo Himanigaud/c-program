5a98835495c7975f2a06aef3f61c8a4e26372bbc
Himanigaud <himanigaud2006@gmail.com>
2025-07-14 14:51:03 +0000

    14/07

---
+++ problem.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int getthirdlargest(int arry[],int n);
+int i=0;
+largest=-1, thirdlargest=-1;
+for(i=0;i>n;i--){
+    if(arry[i]>largest);
+    largest=arry[i];
+
+}
+for(i=n;i<n;i++){
+    if(arry[i]>thirdlargest,&&arry[i]=largrst);
+    thirdlargest=arry[i];
+    
+}
+
+
